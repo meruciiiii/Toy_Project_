edbff3deb1df90079659da1aef5d51a54a9c4a64
Changsikeee <skagns199511@gmail.com>
2025-12-05 16:22:51 +0900

    플레이어 스킬 스크립트 추가

---
+++ Assets/3.Script/C/PlayerB.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerB : PlayerController_C
+{
+    /*
+    [Header("B 스킬 설정")]
+    public float skillDuration = 5f;
+
+    protected override void UseSkill()
+    {
+        StartCoroutine(SlowMotion());
+    }
+
+    IEnumerator SlowMotion()
+    {
+        Debug.Log("B: 선생님 찬스 발동 (과제 속도 저하)");
+
+        //속도 조절 요청
+        // assignmentManager.Instance. // 속도 절반
+
+        yield return new WaitForSeconds(skillDuration);
+
+        // assignmentManager.Instance. // 속도 원복
+        Debug.Log("B: 스킬 종료");
+    }
+    */
+}
---
+++ Assets/3.Script/C/PlayerController_c.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController_C : MonoBehaviour {
+	protected float playerSpeed = 10f;
+
+	[SerializeField] private PlayerInput Input;
+	private Rigidbody player_r;
+
+	[SerializeField] private MapSize size;
+
+	private void Awake() {
+		TryGetComponent(out player_r);
+	}
+
+	private void FixedUpdate() {
+		Move();
+
+		transform.position = new Vector3(
+			Mathf.Clamp(transform.position.x, size.LimitMin.x, size.LimitMax.x),
+			transform.position.y,
+			Mathf.Clamp(transform.position.z, size.LimitMin.z, size.LimitMax.z)
+			);
+
+	}
+
+	private void Move() {
+		Vector3 playerMovement = new Vector3(Input.playerDirection_x, 0f, Input.playerDirection_z) * playerSpeed * Time.deltaTime;
+
+		player_r.MovePosition(player_r.position + playerMovement);
+	}
+	
+}
