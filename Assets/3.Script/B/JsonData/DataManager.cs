0c6f7a4f51ea685fabd3ade87ac496b0188d3730
RiverPark99 <phs201007@gmail.com>
2025-12-05 14:53:12 +0900

    UI,JsonData_beta

--- Assets/3.Script/B/JsonData/DataManager.cs
+++ Assets/3.Script/B/JsonData/DataManager.cs
@@ -38,17 +38,18 @@ public enum Charactor
  */
 
 [Serializable]
-public class Ranking
+public class Ranking //데이터를 여기 안에 담겠습니다.
 {
-    public List<Data> ranking = new List<Data>();
+    public List<Data> Listdata = new List<Data>();
 }
 
+[Serializable]
 public class Data //단판 데이터 이걸 리스트에 담아서 저장하겠습니다.
 {
     //캐릭터의 경우 Enum 으로 관리합시다.
     //플레이어 이름 string (키보드 입력 받아서 저장)
     //기록(시간) float double ->기록에 따라 랭킹 순서
-    //기록의 경우 Time.deltatime -> 시간 초로 변환?
+    //기록의 경우 Time.deltatime -> 시간 초로 변환해서 20:00에서 빼서 높으면 점수가 높은거다.(6:00 칼퇴가 중요하다.)
     
     public string Playername;
     public Charactor charactor;
@@ -60,19 +61,48 @@ public class Data //단판 데이터 이걸 리스트에 담아서 저장하겠습니다.
         this.charactor = charactor;
         this.cleartime = cleartime;
     }
+    /*
+     string nameInput = "플레이어이름";
+     Charactor selectedChar = Charactor.Ppipi;
+     float finalTime = 45.8f; //최종 시간
+
+     Data 객체 생성 시, 생성자에 각각 전달
+     Data newRecord = new Data(nameInput, selectedChar, finalTime);  
+     DataManager의 AddNewRanking(Data newRecord); 로 넣고 랭킹 섞어야 됩니다.
+    이후로는 UI에 리스트 넣고 반복문 이중 포문등으로 Text를 돌려버리면 OK
+    for(int i = 0, i<Ranking.jsonSaveData.Count,i++) // 3번 반복
+        {
+         
+        }
+     */
 }
 
 public class DataManager : MonoBehaviour
 {
+    public static DataManager instance = null;
+
+    //파일 경로체크
     private string filename = "ranking_data.json";
     private static string path;//파일 저장 경로(파일명까지)
 
     private void Awake()
     {
-        path = Path.Combine(Application.persistentDataPath, filename);
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
         
+        path = Path.Combine(Application.persistentDataPath, filename);//파일경로에 파일이름을 합쳐서 string화
+        //persistentDataPath 경로
+        //C:\Users\[user name]\AppData\LocalLow\[company name]\[product name]
+
         //path = Application.persistentDataPath + "tmp.txt";
-        
+
         //SaveToJson(new Data("",0,0f));
         //SaveToJson(new Data("",Charactor.Song, 0f));
     }
@@ -82,31 +112,57 @@ public class DataManager : MonoBehaviour
         if (!File.Exists(path))// path = Path.Combine(Application.persistentDataPath, filename); 파일 명까지의 경로가 없다면
         {
             SaveToJson(new Ranking());
-            Debug.Log("랭킹파일 없음. 새로 생성하겠습니다.");
+            Debug.Log("랭킹파일 없음. 새로 생성.");
         }
     }
+    //파일 경로체크
 
-    public static void SaveToJson(Ranking jsonSaveData)
+    public static void SaveToJson(Ranking ranking) //총 랭킹 데이터 저장 메서드
     {
-        string jsonData = JsonMapper.ToJson(jsonSaveData); //랭킹  
+        string jsonData = JsonMapper.ToJson(ranking); //랭킹데이터를 제이슨으로 바꾸기 전에 string에 넣어서
 
-        // 파일에 저장 Ctrl + S
-        File.WriteAllText(path, jsonData);
+        File.WriteAllText(path, jsonData); //경로(파일)안에 그 string 데이터를 전부 적어넣는다.
         Debug.Log("랭킹 데이터 저장 완료");
     }
 
-    public Data LoadFromJson()//시작할때 로드
+    public Ranking LoadFromJson()//총 랭킹 데이터 불러오기 메서드
     {
         if (!File.Exists(path))
-            return null;
+        {
+            Ranking newRanking = new Ranking();// return하기 위해 따로 지역변수로 담아 선언
+            SaveToJson(newRanking);
+            Debug.Log("랭킹 파일 없음. 새로 생성하고 빈 랭킹 객체 반환");
+            return newRanking;
+        }
 
         // 파일을 불러옴
-        string jsonData = File.ReadAllText(path);
+        string jsonData = File.ReadAllText(path); // 경로(파일)안에 그 string 데이터를 전부 받아 넣겠습니다. return하기 위해 지역변수로 담아 선언
 
-        // B -> C가는 작업
         //Data jsonSaveData = JsonUtility.FromJson<Data>(jsonData);
-        Data jsonSaveData = JsonMapper.ToObject<Data>(jsonData);
+        Ranking jsonSaveData = JsonMapper.ToObject<Ranking>(jsonData);
 
         return jsonSaveData;
+    }
+
+    // 랭킹 반영 로직
+
+    public int Ranking_count = 4; //4위까지만 기록하겠습니다.
+    public void AddNewRanking(Data newrecord) //플레이 Data를 랭킹에 추가하고 자동으로 지우는 메서드
+    {
+        Ranking rankingdata = LoadFromJson(); //Ranking 클래스를 일단 불러오고
+        rankingdata.Listdata.Add(newrecord);//그 클래스에 있는 안에 있는 변수(리스트를)에 Data를 먼저 넣고 그 이후에 섞겠습니다. 나중에 저장도 해야됩니다.
+        rankingdata.Listdata.Sort((a, b) => a.cleartime.CompareTo(b.cleartime));//List.Sort 정렬알고리즘 메서드
+
+        if (rankingdata.Listdata.Count > Ranking_count)
+        {
+            rankingdata.Listdata.RemoveAt(Ranking_count);//4위 이후는 지워버리겠습니다.
+        }
+        SaveToJson(rankingdata);
+    }
+
+    public List<Data> GetRankingList() //밖으로 데이터 빼고 싶을때.
+    {
+        Ranking rankingData = LoadFromJson(); // 파일에서 데이터를 로드하거나 초기화합니다.
+        return rankingData.Listdata;
     }
 }
